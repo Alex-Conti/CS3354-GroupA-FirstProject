ee6848c34efa5a68a5be6b4760767ecf2ccb904c
Alex <tso24@txstate.edu>
2026-03-23 19:15:24 -0500

    [Prompt] Asked for movement positions for knight; [Adopt] Knight movement pattern; [Change] Knight can now move correctly

--- src/pieces/Knight.java
+++ src/pieces/Knight.java
@@ -14,7 +14,27 @@ public class Knight extends Piece {
     @Override
     public List<Position> possibleMoves() {
         List<Position> moves = new ArrayList<>();
-        // TODO: Implement 'L' shape movement logic
+        int row = position.getRow();
+        int col = position.getColumn();
+
+        // Possible moves
+        int[][] knightMoves = {
+            {row - 2, col - 1}, {row - 2, col + 1},
+            {row + 2, col - 1}, {row + 2, col + 1},
+            {row - 1, col - 2}, {row - 1, col + 2},
+            {row + 1, col - 2}, {row + 1, col + 2}
+        };
+
+        for (int[] move : knightMoves) {
+            int r = move[0];
+            int c = move[1];
+
+            // Checks boundaries
+            if (r >= 0 && r < 8 && c >= 0 && c < 8) {
+                moves.add(new Position(r, c));
+            }
+        }
+
         return moves;
     }
 }
